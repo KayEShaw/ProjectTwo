5ca7922b814ab24e85f716aad8c002cb00cbc1e6
Amwaj <amwajsabri@gmail.com>
2025-02-27 16:15:35 -0600

    Created MiddeEarthCouncil classwith constructors and methods

--- src/residents/MiddleEarthCouncil.java
+++ src/residents/MiddleEarthCouncil.java
@@ -1,5 +1,26 @@
 package residents;
 
-public class MiddleEarthCouncil {
+import managment.CharacterManager;
 
+public class MiddleEarthCouncil {
+	private static MiddleEarthCouncil instance;
+	private CharacterManager characterManager; //create instance of CharacterManager 
+	
+	//Private constructor so no one can create an instance of the class
+	private MiddleEarthCouncil() {
+		this.characterManager =  new CharacterManager(); //initialize when council is instance is made
+	}
+
+	//This is a public method to get the instance 
+	public static MiddleEarthCouncil getInstance() {
+		if(instance == null) {
+			instance = new MiddleEarthCouncil();
+		} 
+		return instance;
+	}
+
+	//Method to provide access to the CharacterManager class
+	public CharacterManager getCharacterManager() {
+		return characterManager;
+	}
 }
