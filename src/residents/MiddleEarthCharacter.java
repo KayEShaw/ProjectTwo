21831db23540eabe29cb615f21aa31207e0dadf0
KayEShaw <198162631+KayEShaw@users.noreply.github.com>
2025-02-28 12:27:25 -0600

    added methods

--- src/residents/MiddleEarthCharacter.java
+++ src/residents/MiddleEarthCharacter.java
@@ -1,5 +1,54 @@
 package residents;
 
-public class MiddleEarthCharacter {
+//this is a abstract class for the middle earth characters
+abstract class MiddleEarthCharacter {
 
+private String characterName;
+private double healthStat;
+private double powerLvl;
+
+//CONSTRUCTORS
+public MiddleEarthCharacter() {
+	System.out.println("character created");
+}
+
+public MiddleEarthCharacter(String name, double health, double power) {
+	this.characterName = name;
+	this.healthStat = health;
+	this.powerLvl = power;
+}
+
+//ABSTRACT METHODS
+abstract boolean attack(MiddleEarthCharacter target);
+abstract String getRace();
+
+//GETTERS AND SETTERS
+public String getCharacterName() {
+	return characterName;
+}
+
+public void setCharacterName(String characterName) {
+	this.characterName = characterName;
+}
+
+public double getHealthStat() {
+	return healthStat;
+}
+
+public void setHealthStat(double healthStat) {
+	this.healthStat = healthStat;
+}
+
+public double getPowerLvl() {
+	return powerLvl;
+}
+
+public void setPowerLvl(double powerLvl) {
+	this.powerLvl = powerLvl;
+}
+
+//METHODS
+void displayInfo() {
+ 
+}
 }
