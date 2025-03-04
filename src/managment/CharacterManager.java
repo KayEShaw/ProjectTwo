651f3e494a28cc2a4d0e0644d87ec8b38f7fe03a
Amwaj <amwajsabri@gmail.com>
2025-03-04 01:43:29 -0600

    added getCharacter, updateCharacter, and deleteCharacter

--- src/managment/CharacterManager.java
+++ src/managment/CharacterManager.java
@@ -44,5 +44,87 @@ public class CharacterManager {
 		return true;
 	}
 	
+	/**
+	 * This method goes through the characters added in the array
+	 * and finds the characters that match the name 
+	 * @param name; the name of the character to find 
+	 * @return; returns character at certain index given the name 
+	 */
+	MiddleEarthCharacter getCharacter(String name) {
+		for(int i = 0; i < size; i++) {
+			if(characters[i] != null && characters[i].getCharacterName().equals(name)) {
+				return characters[i];
+			}
+		}
+		return null;
+	} 
 	
+	
+	/**
+	 * This method updates the information (name, health, power) of an existing character in the array, 
+	 * if any update is made, it returns true. If no changes are made or if the character is not found 
+	 * it returns false.
+	 *  
+	 * @param character
+	 * @param name
+	 * @param health
+	 * @param power
+	 * @return
+	 */
+	boolean updateCharacter(MiddleEarthCharacter character, String name, int health, int power) {
+		for(int i = 0; i < size; i++) {
+			if(characters[i] != null && characters[i].getCharacterName().equals(character.getCharacterName())) {
+				
+				//check if name needs to be updated
+				if(!characters[i].getCharacterName().equals(name)) {
+					characters[i].setCharacterName(name); //update the name
+					return true;
+				}
+				
+				if(characters[i].getHealthStat() != health) {
+					characters[i].setHealthStat(health); //update health
+					return true;
+				}
+				
+				if(characters[i].getPowerLvl() != power) {
+					characters[i].setPowerLvl(power);
+					return true; 
+				}
+			}
+		}
+		return false; 
+	} 
+	
+	/**
+	 * This method checks if a character is in the array, and shifts all elements 
+	 * on the right hand side to the left side one. Set the last index to null for 
+	 * not duplicates.Reduce the size.
+	 * @param character
+	 * @return; return true if character is removed and size is decremted otherwise its false
+	 */
+	boolean deleteCharacter(MiddleEarthCharacter character) {
+		for(int i = 0; i < size; i++) {
+			if(characters[i] != null && characters[i].equals(character)) {
+				for(int j = 0; j < size - 1; i++) {
+					characters[j] = characters[j+1];
+				}
+				characters[size-1] = null; //set last element to null
+				size--; //reduce size 
+				return true; 
+			}
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * This method displays all the characters in the array 
+	 */
+	void displayAllCharacters() {
+		for(int i = 0; i < size; i++) {
+			if(characters[i] != null) {
+				//add display info 
+			}
+		}
+	}
 }
