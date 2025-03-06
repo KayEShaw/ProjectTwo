d8b285cd7e6f98506c54b266ba6fcab486a7cd82
Amwaj <amwajsabri@gmail.com>
2025-03-05 23:31:46 -0600

    added Elf class javadoc

--- src/residents/Elf.java
+++ src/residents/Elf.java
@@ -1,16 +1,38 @@
 package residents;
 
 public class Elf extends MiddleEarthCharacter{
+	
+	/**
+	 * This is a parameterized constructor that helps the Elf class initialize 
+	 * it's own parameters by referencing the superclass constructor using "super(name, health, power)"
+	 * @param name; name of character
+	 * @param health; health of character
+	 * @param power; power of character
+	 */
 	//CONSTRUCTOR
 	public Elf(String name, double health, double power) {
 		super(name, health, power);
 		System.out.println("Elf character created ("+ name +")");
 	}
 	
+	/**
+	 * This method returns the type of character
+	 */
 	@Override
 	public String getRace() {
 		return "Elf";
 	}
+	
+	/**
+	 * This method creates an attack by the Elf class on different character type. It shows the 
+	 * damage done to each different race.
+	 *  
+	 * Elf does 1.5x it's power as damage to an Orc
+	 * Elf does damage equal to it's power level to a Human or Wizard
+	 * If the race is unknown, it does no damage
+	 * 
+	 * returns true if damage is done, false if not 
+	 */
 	@Override 
 	public boolean attack(MiddleEarthCharacter target) {
 		if(target.getRace().equals("Orc")) {
